0362084e5c5cc6463cba1a790c8ea0fcb33c8bab
akshay1580 <74129699+akshay1580@users.noreply.github.com>
2021-05-28 13:23:50 +0530

    K&R 4 Spaces

--- guessthenumer.c
+++ guessthenumer.c
@@ -1,4 +1,4 @@
-/* Program to guess a random number using relational operator */
+/* Program to guess the number */
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -6,22 +6,22 @@
 
 int main()
 {
-      int number, noofguesses=1, guess;
-      srand(time(0));
-      number= rand()%100 + 1;
-      printf("The number is %d\n" , number);
-      do {
-          printf("Guess the number between 1 to 100\n");
-          scanf("%d", &guess);
-         
-          if (guess > number) {
-              printf("Lower number please\n");
-          } else if(guess < number) {
-              printf("Higher number please\n");
-          } else {
-              printf("You guessed it in %d attempts\n " , noofguesses);
-          }
-          noofguesses++;
-      } while (guess!=number);
-      return 0;
-}
+    int number, noofguesses = 1, guess;
+    srand(time(0));
+    number = rand()%100 + 1;
+    printf("The number is %d\n" , number);
+    do {
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+          
+        if (guess > number) {
+            printf("Lower number please\n");
+        } else if ( guess< number) {
+            printf("Higher number please\n");
+        } else {
+            printf("You guessed it in %d attempts\n " , noofguesses);
+        }
+        noofguesses++;
+    } while (guess != number);
+    return 0;
+  }
